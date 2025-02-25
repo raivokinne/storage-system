72cd51e4363bd60b1575b53b37fffbe08242227f
Chee3se <emilspete999@gmail.com>
2025-02-25 12:27:57 +0200

    Style fix

--- views/components/footer.php
+++ views/components/footer.php
@@ -1,2 +1,10 @@
+</main>
+<footer class="bg-white shadow-md mt-auto b-0">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div class="flex justify-center items-center">
+            <p class="text-sm text-gray-700">&copy; <?php echo date('Y'); ?> Storage. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
--- views/index.view.php
+++ views/index.view.php
@@ -1,6 +1,6 @@
 <?php component('header'); ?>
 <div class="container mx-auto w-full text-center pt-32">
-    <h1 class="text-6xl font-bold text-blue-900 pt-6">Storage</h1>
-    <p class="text-xl font-bold pt-6 text-blue-700">Cool ass storage system</p>
+    <h1 class="text-8xl font-extrabold bg-gradient-to-r pb-4 from-blue-500 via-blue-700 to-purple-900 text-transparent bg-clip-text pt-6">Storage System</h1>
+    <p class="text-2xl font-medium text-blue-800 pt-4 bg-clip-text">Just a cool ass storage system</p>
 </div>
 <?php component('footer'); ?>
