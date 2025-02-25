b7270a45f2feb45cd70a3717805805aea60c0668
Raivo Kinne <120594466+raivokinne@users.noreply.github.com>
2025-02-25 10:03:35 +0200

    Emils (#10)
    * Index
    
    * Home text but blue
    
    * Favicon and some other bs
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>

---
+++ views/components/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta lang="en" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+    <title>Storage</title>
+</head>
+<body>
---
+++ views/index.view.php
@@ -0,0 +1,6 @@
+<?php component('header'); ?>
+<div class="container mx-auto w-full text-center pt-32">
+    <h1 class="text-6xl font-bold text-blue-900 pt-6">Storage</h1>
+    <p class="text-xl font-bold pt-6 text-blue-700">Cool ass storage system</p>
+</div>
+<?php component('footer'); ?>
