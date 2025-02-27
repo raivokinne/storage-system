f62c26794b17a8a069e43b6129bdc7fb6b4b3778
Raivo Kinne <120594466+raivokinne@users.noreply.github.com>
2025-02-27 17:33:12 +0200

    Es (#22)
    * feat: fuck the database and models
    
    * Emils (#10)
    
    * Index
    
    * Home text but blue
    
    * Favicon and some other bs
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>
    
    * feat: idk
    
    * chore: removde unsude files form core
    
    * fix: fixed the f nameing scheme
    
    * Es (#19) (#20)
    
    * feat: fuck the database and models
    
    * Emils (#10)
    
    * Index
    
    * Home text but blue
    
    * Favicon and some other bs
    
    ---------
    
    
    
    * feat: idk
    
    * chore: removde unsude files form core
    
    * fix: fixed the f nameing scheme
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>
    
    * Es (#19)
    
    * feat: fuck the database and models
    
    * Emils (#10)
    
    * Index
    
    * Home text but blue
    
    * Favicon and some other bs
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>
    
    * feat: idk
    
    * chore: removde unsude files form core
    
    * fix: fixed the f nameing scheme
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>
    
    * feat: adding login
    
    * fix: the router needs a upgrade
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 const BASE_PATH = __DIR__.'/../';
-
 session_start();
-
 require BASE_PATH . 'vendor/autoload.php';
 require BASE_PATH . 'core/functions.php';
 
@@ -11,13 +10,11 @@ $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH, '.env');
 $dotenv->load();
 
 use Database\Database;
-
 $config = require BASE_PATH . 'config/database.php';
-
 Database::connect($config);
 
 $router = new \Core\Router;
-require BASE_PATH . '/routes/web.php';
+require BASE_PATH . 'routes/web.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
@@ -25,5 +22,8 @@ $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 try {
     $router->route($uri, $method);
 } catch (\Exception $e) {
-    return redirect($router->previousUrl());
+    echo "Error: " . $e->getMessage();
+    if (function_exists('redirect')) {
+        return redirect($router->previousUrl());
+    }
 }
