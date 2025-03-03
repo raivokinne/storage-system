2f02c9795d302dee2f191b406246645660e5feeb
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2025-03-03 13:42:08 +0200

    feat: fuck this bulshit ass file upload kil yout fucking self whit this bulshit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Controllers\Auth\SessionController;
 use App\Controllers\Auth\UserController;
 use App\Controllers\PageController;
+use App\Controllers\TestController;
 
 global $router;
 
@@ -16,3 +17,6 @@ $router->post('/register', [UserController::class, 'store']);
 $router->get('/profile', [UserController::class, 'show']);
 
 $router->get('/logout', [SessionController::class, 'destroy']);
+
+$router->post('/upload', [TestController::class, 'test']);
+$router->get('/upload', [TestController::class, 'idk']);
