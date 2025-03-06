6daeed4ad6e3bed9bbe192f6ff4e5915a97eb34d
Raivo Kinne <120594466+raivokinne@users.noreply.github.com>
2025-03-06 22:43:31 +0200

    Emils (#42)
    * Index
    
    * Home text but blue
    
    * Favicon and some other bs
    
    * Quick fix
    
    * Style fix
    
    * Minor design tweaks
    
    * Even more minor design tweaks
    
    * PHPStorm suggestions to tweak the code (tested, real)
    
    * Login with font-end and back-end validation (i hate myself)
    
    * Forgot to put old in views
    
    * Merge fix, im too tired, if im reading this MAKE CHANGES ASAP
    
    * lalal
    
    * big
    
    * yes
    
    * Image
    
    * Button
    
    * Profile pic
    
    * Finished the shitty pfp code
    
    * External image input working
    
    * Final fix?
    
    * Fix
    
    * QR + fix
    
    * Auto action
    
    * Crucial fix
    
    ---------
    
    Co-authored-by: Chee3se <emilspete999@gmail.com>
    Co-authored-by: Chee3se <125540992+Chee3se@users.noreply.github.com>

--- views/index.view.php
+++ views/index.view.php
@@ -2,6 +2,6 @@
 <div class="container mx-auto w-full text-center pt-32">
     <h1 class="text-8xl font-extrabold bg-gradient-to-r pb-4 from-blue-500 via-blue-700 to-purple-900 text-transparent bg-clip-text pt-6">Storage System</h1>
     <p class="text-2xl font-medium text-blue-800 pt-4 bg-clip-text">Just a cool ass storage system</p>
-    <img src="<?= (new \chillerlan\QRCode\QRCode())->render('If you are reading this you are gay') ?>" alt="QR Code" class="w-48 mx-auto mt-4 border "/>
+    <img src="<?= (new \chillerlan\QRCode\QRCode())->render('If you are reading this you are gay') ?>" alt="QR Code" class="w-48 mx-auto mt-8 border "/>
 </div>
 <?php component('footer'); ?>
