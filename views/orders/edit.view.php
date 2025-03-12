46c5d5b4db79c429ed254571c132c3edf00ea97b
XIILumina <zibrevs@inbox.lv>
2025-03-12 12:42:25 +0200

    Alberts (#49)
    * actions, products, orders, shelves index view and controllers
    
    * CRUD for Products/Shelves/Orders, jaievieto kkads Suppliers caur db lai strada
    
    * updated orderscontroller
    
    * idk
    
    ---------
    
    Co-authored-by: XIILumina <playstation9@Inbox.lv>
    Co-authored-by: Raivo Kinne <120594466+raivokinne@users.noreply.github.com>

--- views/orders/edit.view.php
+++ views/orders/edit.view.php
@@ -21,9 +21,9 @@
         <div class="w-1/2 mt-4">
             <label for="status" class="block text-gray-700 text-sm font-bold mb-2">Status:</label>
             <select name="status" id="status" class="w-full border py-1 px-4 my-2 rounded-xl" required>
-                <option value="pending"                                                                               <?php echo $order['status'] == 'pending' ? 'selected' : '' ?>>Pending</option>
-                <option value="completed"                                                                                   <?php echo $order['status'] == 'completed' ? 'selected' : '' ?>>Completed</option>
-                <option value="cancelled"                                                                                   <?php echo $order['status'] == 'cancelled' ? 'selected' : '' ?>>Cancelled</option>
+                <option value="pending"                                                                                                                      <?php echo $order['status'] == 'pending' ? 'selected' : '' ?>>Pending</option>
+                <option value="completed"                                                                                                                            <?php echo $order['status'] == 'completed' ? 'selected' : '' ?>>Completed</option>
+                <option value="cancelled"                                                                                                                            <?php echo $order['status'] == 'cancelled' ? 'selected' : '' ?>>Cancelled</option>
             </select>
         </div>
         <button type="submit" class="mt-6 bg-gradient-to-br from-blue-400 to-purple-500 rounded px-6 py-2 text-white font-bold hover:to-blue-500 hover:from-purple-600 transition hover:scale-105 duration-400">Update Order</button>
